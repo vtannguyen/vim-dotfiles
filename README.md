c1dd3d6cbf87d8649984620c751225a4996e1dbc
Tan Nguyen <tantit91@gmail.com>
2021-04-23 19:49:26 +0200

    add script to install caps2esc

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ This repository consists of all config files for better vim usage.
 ## Installation
 This intallation guide is for Fedora 30
 
-* First you should remap CAPS to ESCAPE and CTRL (when held) key by following the instructions
-in http://nelsonware.com/blog/2019/04/30/how-to-map-caps-lock-to-escape-and-control-on-fedora-via-caps2esc.html
+* To remap CAPS to ESCAPE and CTRL (when held) key, please run the script `install_caps2esc.sh`. The script is written following the instructions in [How to map capslock to escape and control](http://nelsonware.com/blog/2019/04/30/how-to-map-caps-lock-to-escape-and-control-on-fedora-via-caps2esc.html)
+
 
 * Then install wl-clipboard for system clipboard copy and paste
 `sudo dnf install wl-clipboard`
---
+++ install_caps2esc.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+mkdir tmp
+cd tmp
+
+sudo dnf install git -y
+
+git clone https://gitlab.com/interception/linux/tools interception_tools
+git clone https://gitlab.com/interception/linux/plugins/caps2esc
+
+# Build and install interception tools
+cd interception_tools
+sudo dnf install cmake libevdev-devel systemd-devel yaml-cpp-devel boost-devel g++ -y
+cmake -B build -DCMAKE_BUILD_TYPE=Release
+cmake --build build
+
+cd build && sudo make install
+
+# Build and install caps2esc
+cd ../../caps2esc
+cmake -B build -DCMAKE_BUILD_TYPE=Release
+cmake --build build
+cd build && sudo make install
+
+sudo tee -a /etc/caps2esc.yaml > /dev/null <<EOF
+- JOB: "intercept -g $DEVNODE | caps2esc | uinput -d $DEVNODE"
+  DEVICE:
+    EVENTS:
+      EV_KEY: [KEY_CAPSLOCK, KEY_ESC]
+EOF
+
+sudo tee -a /etc/systemd/system/caps2esc.service > /dev/null <<EOF
+[Unit]
+Description=caps2esc
+
+[Service]
+ExecStart=/usr/bin/nice -n -20 /usr/local/bin/udevmon -c /etc/caps2esc.yaml
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable caps2esc
+sudo systemctl start caps2esc
+
+# Clean up
+cd ../../../
+rm -rf tmp
