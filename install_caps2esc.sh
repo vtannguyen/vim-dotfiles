730dd910a4c89ab839737d64248de337a5ea9e27
Tan Nguyen <tantit91@gmail.com>
2022-10-31 06:25:41 -0400

    fix bug install remapping cap2esc

--- install_caps2esc.sh
+++ install_caps2esc.sh
@@ -23,7 +23,7 @@ cmake --build build
 cd build && sudo make install
 
 sudo tee -a /etc/caps2esc.yaml > /dev/null <<EOF
-- JOB: "intercept -g $DEVNODE | caps2esc | uinput -d $DEVNODE"
+- JOB: "intercept -g \$DEVNODE | caps2esc | uinput -d \$DEVNODE"
   DEVICE:
     EVENTS:
       EV_KEY: [KEY_CAPSLOCK, KEY_ESC]
